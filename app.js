ea108d904cf9aae6954539f59ce4223feaa4fddb
carlosvieira <carlosvieira278@gmail.com>
2022-03-31 10:10:26 +0200

    last back update

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors')
 const quizRouter = require('./routes/quiz.js');
 const app = express();
 const port = 3000;
 
+app.use(cors())
 app.use('/', quizRouter);
 
 app.listen(port, () => {
